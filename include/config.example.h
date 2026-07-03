6e080174e215256de98fd14db6d14fd4f25c3ac8
Daniel <danielcizmar3012@gmail.com>
2026-07-03 15:28:26 +0200

    added files for 3D printing and modified config example

--- include/config.example.h
+++ include/config.example.h
@@ -3,6 +3,14 @@
 
 #define WIFI_SSID "your_wifi_name"
 #define WIFI_PASSWORD "your_wifi_password"
-// API TOKENS, ...
+
+#define PZEM_RX_PIN "rx_pin_number"
+#define PZEM_TX_PIN "tx_pin_number"
+#define TZ_INFO "your_timezone_info"
+#define DATA_READ_INTERVAL "your_data_read_interval"
+
+#define INFLUXDB_URL "your_influxdb_url"
+#define INFLUXDB_ORG "your_influxdb_organization"
+#define INFLUXDB_TOKEN "your_influxdb_token"
 
 #endif
